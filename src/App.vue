7c5c514fb2ab6329dc3bad33348c892e7b126410
TALEX1995 <tanase.alex@libero.it>
2023-07-10 15:17:57 +0200

    Added header components

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,17 @@
+<script>
+import AppHeader from './components/AppHeader.vue'
 
+export default {
+    components: {
+        AppHeader,
+    },
+}
+</script>
+
+
+<template>
+    <AppHeader />
+</template>
+
+
+<style></style>
