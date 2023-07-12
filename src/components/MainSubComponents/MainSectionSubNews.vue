3639d75993df74c28219c9e073b78a6ddc489d2a
TALEX1995 <tanase.alex@libero.it>
2023-07-12 12:54:52 +0200

    Added input section sub news

--- src/components/MainSubComponents/MainSectionSubNews.vue
+++ src/components/MainSubComponents/MainSectionSubNews.vue
@@ -12,7 +12,7 @@
                 <!-- Form -->
                 <div class="col">
                     <form @submit.prevent>
-                        <input type="e-mail">
+                        <input class="ps-4" type="e-mail" placeholder="Email address...">
                     </form>
                 </div>
             </div>
@@ -33,5 +33,13 @@ section {
 
 h1 {
     color: $white;
+}
+
+input {
+    width: 80%;
+    margin-left: $rem5;
+    line-height: $rem4;
+    border-radius: 40px;
+    border: none;
 }
 </style>
